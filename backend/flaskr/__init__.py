4ef3388e7400637c2f31991a3ed700cd9877bd14
Alf Graham <alfgraham@mac.com>
2020-09-29 22:36:26 +0100

    Corrected CORS headers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -21,15 +21,15 @@ def create_app(test_config=None):
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
   def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers',
-                          'Content-Type, Authorization, true')
-    response.headers.add('Access-Control-Allow-Methods',
-                          'GET, POST, PATCH, DELETE, OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials',
-                          'true')
-
-    return response
+      response.headers.add('Access-Control-Allow-Headers',
+                            'Content-Type, Authorization, true')
+      response.headers.add('Access-Control-Allow-Methods',
+                            'GET, POST, PATCH, DELETE, OPTIONS')
+      response.headers.add('Access-Control-Allow-Credentials',
+                            'true')
+      return response
 
   '''
   @TODO: 
